4bab73cd641a179c3170df3ac540a1d06aa36eea
ezborovskaya <ezborovskaya@EZBOROVSKAYA>
2018-07-04 17:43:19 +0300

    ContactCreationTests

--- addressbook-web-tests1/addressbook-web-tests1/ContactCreationTests.cs
+++ addressbook-web-tests1/addressbook-web-tests1/ContactCreationTests.cs
@@ -49,7 +49,11 @@ namespace WebAddressbookTests
             GoToHomepage();
             Login(new AccountData("admin","secret"));
             CreationContact();
-            InputContactForm("f", "f", "f", "f");
+            ContactData group = new ContactData ("f");
+            group.Firstname = "f";
+            group.Middlename = "f";
+            group.Lastname = "f";
+            group.Nickname = "f";
             // ERROR: Caught exception [Error: Dom locators are not implemented yet!]
             Logout();
         }
@@ -59,16 +63,16 @@ namespace WebAddressbookTests
             driver.FindElement(By.LinkText("Logout")).Click();
         }
 
-        private void InputContactForm(string firstname, string middlename, string lastname, string nickname)
+        private void InputContactForm(ContactData contact)
         {
             driver.FindElement(By.Name("firstname")).Clear();
-            driver.FindElement(By.Name("firstname")).SendKeys(firstname);
+            driver.FindElement(By.Name("firstname")).SendKeys(contact.Firstname);
             driver.FindElement(By.Name("middlename")).Clear();
-            driver.FindElement(By.Name("middlename")).SendKeys(middlename);
+            driver.FindElement(By.Name("middlename")).SendKeys(contact.Middlename);
             driver.FindElement(By.Name("lastname")).Clear();
-            driver.FindElement(By.Name("lastname")).SendKeys(lastname);
+            driver.FindElement(By.Name("lastname")).SendKeys(contact.Lastname);
             driver.FindElement(By.Name("nickname")).Clear();
-            driver.FindElement(By.Name("nickname")).SendKeys(nickname);
+            driver.FindElement(By.Name("nickname")).SendKeys(contact.Nickname);
         }
 
         private void CreationContact()
