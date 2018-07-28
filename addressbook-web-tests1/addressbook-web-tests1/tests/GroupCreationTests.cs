45cd1a00b09828f60c439e5ced4328def7ff8b8c
ezborovskaya <ezborovskaya@EZBOROVSKAYA>
2018-07-28 13:05:26 +0300

    6 лекция

--- addressbook-web-tests1/addressbook-web-tests1/tests/GroupCreationTests.cs
+++ addressbook-web-tests1/addressbook-web-tests1/tests/GroupCreationTests.cs
@@ -10,6 +10,7 @@ using OpenQA.Selenium.Support.UI;
 using System.Xml;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
+using Excel = Microsoft.Office.Interop.Excel;
 
 
 namespace WebAddressbookTests
@@ -64,8 +65,32 @@ namespace WebAddressbookTests
 
         }
 
+        public static IEnumerable<GroupData> GroupDataFromExcelFile()
+        {
 
-        [Test, TestCaseSource("GroupDataFromJsonFile")]
+            List<GroupData> groups = new List<GroupData>();
+            Excel.Application app = new Excel.Application();
+            Excel.Workbook wb = app.Workbooks.Open(Path.Combine(Directory.GetCurrentDirectory(), @"groups.xlsx"));
+            Excel.Worksheet sheet = wb.ActiveSheet;
+            Excel.Range range = sheet.UsedRange;
+            for (int i = 1; i<= range.Rows.Count; i++)
+            {
+                groups.Add(new GroupData()
+                {
+                    Name = range.Cells[i, 1].Value,
+                    Header = range.Cells[i, 2].Value,
+                    Footer = range.Cells[i, 3].Value
+                });
+            }
+            wb.Close();
+            app.Visible = false;
+            app.Quit();
+            return groups;
+
+        }
+
+
+        [Test, TestCaseSource("GroupDataFromExcelFile")]
 
         public void GroupCreationTest(GroupData newData)
         {
